9daddee5131001f9550a12a9017ee3d71377dd66
scott-gianan <scott.gianan@gmail.com>
2022-11-01 13:57:04 +0800

    added sounds. partially complete

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ let playerChoice = [];
 let computerChoice = [];
 
 
-
 const getComputerChoice = () => {
     const choice = ['rock','paper','scissors'];
     let randomChoice = Math.round(Math.random()*2);
@@ -15,56 +14,105 @@ const getComputerChoice = () => {
     
     
 $('.choice').click((event)=> {
+    let option = event.target.id;
+    let audio = new Audio('./sounds/choice.mp3');
+    audio.play();
+
     getComputerChoice();
-    playerChoice.push(event.target.alt);
-    console.log(`Player: ${playerChoice}`);
-    console.log(`Computer: ${computerChoice}`);
+    playerChoice.push(option);
+    highlightPlayerChoice(option);
+    console.log(option);
+    // console.log(`Player: ${playerChoice}`);
+    // console.log(`Computer: ${computerChoice}`);
 })
 
+const showComputerChoice = (choice) => {
+    $('#computer-choice').html(`<img src="./images/${choice}.png" id="rock" alt="rock" width="100">`);
+}
+
+const highlightPlayerChoice = (choice) => {
+    $(`#${choice}`).addClass('pressed');
+    setTimeout(() => {
+    $(`#${choice}`).removeClass('pressed')
+    }, 500);
+}
 
 const play = () => {
 
     let playerLastIndex = playerChoice[playerChoice.length-1];
     let computerLastIndex = computerChoice[computerChoice.length-1];
-
+    
     if(playerLastIndex === 'rock' && computerLastIndex ==='scissors'){
         playerScore++;
+        showComputerChoice(computerLastIndex);
+        audio_win();
         $('.result').text('You won!');
         $('.player-score').text(`${playerScore}`);
     }
     else if(playerLastIndex === 'scissors' && computerLastIndex ==='paper'){
         playerScore++;
+        showComputerChoice(computerLastIndex);
+        audio_win();
         $('.result').text('You won!');
         $('.player-score').text(`${playerScore}`);
     }
     else if(playerLastIndex === 'paper' && computerLastIndex ==='rock'){
         playerScore++;
+        showComputerChoice(computerLastIndex);
+        audio_win();
         $('.result').text('You won!');
         $('.player-score').text(`${playerScore}`);
     }
     else if(computerLastIndex === 'rock' && playerLastIndex ==='scissors'){
         computerScore++;
-        $('.result').text('computer won!');
+        audio_lost();
+        showComputerChoice(computerLastIndex);
+        $('.result').text('You Lost!');
         $('.computer-score').text(`${computerScore}`);
     }
     else if(computerLastIndex === 'scissors' && playerLastIndex ==='paper'){
         computerScore++;
-        $('.result').text('computer won!');
+        audio_lost();
+        showComputerChoice(computerLastIndex);
+        $('.result').text('You Lost!');
         $('.computer-score').text(`${computerScore}`);
     }
     else if(computerLastIndex === 'paper' && playerLastIndex ==='rock'){
         computerScore++;
-        $('.result').text('computer won!');
+        audio_lost();
+        showComputerChoice(computerLastIndex);
+        $('.result').text('You Lost!');
         $('.computer-score').text(`${computerScore}`);
     }
     else if(computerLastIndex===playerLastIndex){
+        audio_draw();
+        showComputerChoice(computerLastIndex);
         $('.result').text('Draw!');
     }
 }
 
-$('button').click(play);
+$('button').click(()=> {
+    play();
+    setTimeout(() => {
+        playerChoice=[];
+    }, 100);
+});
 
-const showComputerChoice = (choice) => {
 
+//Sound for losing a round:
+const audio_lost = () => {
+    let lost = new Audio('./sounds/lost.mp3');
+    lost.play();
 }
 
+//Sound for winning a round:
+const audio_win = () => {
+    let win = new Audio('./sounds/win.mp3');
+    win.play();
+}
+
+//Sound for when the round is a draw
+const audio_draw = () => {
+    let draw = new Audio('./sounds/draw.mp3');
+    draw.play();
+}
